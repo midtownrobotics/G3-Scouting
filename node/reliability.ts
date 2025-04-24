367ce8d27e0afc80a600c89043ddefe09a8c1fb6
Gray486 <me@grayjn.com>
2025-04-24 09:24:24 -0400

    fully clean for rewrite

--- node/reliability.ts
+++
@@ -1,9 +0,0 @@
-import schedule from "node-schedule";
-
-const matchesNoData: number[] = []
-
-export const scheduleMatchForReliabilityGrading = (match: number) => matchesNoData.push(match);
-
-schedule.scheduleJob("", () => {
-    
-})
